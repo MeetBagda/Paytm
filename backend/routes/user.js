fdb15ea8c956fb00f4ed45b3e2f9096bdddcb7ee
MeetBagda <bagdameet@gmail.com>
2024-12-27 16:06:57 +0530

    add: account schema & transaction logic added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const z = require("zod");
 const jwt = require("jsonwebtoken");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const JWT_SECRET = require("../config");
+const { authMiddleware } = require("../middleware");
 const router = express.Router();
-
 
 //sign-up
 const signUpSchema = z.object({
@@ -33,6 +33,13 @@ router.post("/sign-up", async (req, res) => {
   }
 
   const dbUser = await User.create(body);
+
+  //   ---------------add some random money to user account----------------
+  await Account.create({
+    userId: dbUser._id,
+    balance: 1 + Math.random() * 10000,
+  });
+
   const token = jwt.sign(
     {
       userId: dbUser._id,
@@ -80,6 +87,51 @@ router.post("/sign-in", async (req, res) => {
   });
 });
 
-router.get("/", (req, res) => {});
+//update user
+const updateBody = z.object({
+  password: z.string().optional(),
+  firstname: z.string().optional(),
+  lastname: z.string().optional(),
+});
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    return res
+      .status(400)
+      .json({ message: "Error while updating user information" });
+  }
 
+  await User.updateOne(req.body, {
+    id: req.userId,
+  });
+
+  res.json({ message: "User information updated successfully" });
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+    $or: [
+      {
+        firstname: {
+          $regex: filter,
+        },
+      },
+      {
+        lastname: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      _id: user._id,
+    })),
+  });
+});
 module.exports = router;

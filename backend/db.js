51926d224cb888e9b03485c6843acd0b412a3d80
MeetBagda <bagdameet@gmail.com>
2024-12-28 17:39:16 +0530

    add: routes are added, transaction logic added, middleware added

--- backend/db.js
+++ backend/db.js
@@ -1,32 +1,54 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://mjbagda035:paytm-clone-cluster@paytm-cluster.7k5ic.mongodb.net/")
+mongoose.connect(
+  "mongodb+srv://mjbagda035:paytm-clone-cluster@paytm-cluster.7k5ic.mongodb.net/"
+);
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        trim: true,
-        lowercase: true,
-        minLength:3,
-        maxLength:30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    typee: Number,
+    required: true,
+  },
+});
+
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+  Account,
+};
